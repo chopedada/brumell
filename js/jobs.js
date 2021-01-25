584fc5883dda914cc70f696d173432fe985516f9
Utsav Kafley <vastukafley@gmail.com>
2021-01-25 13:17:44 -0500

    Finalized layout for all pages, application page form and email completed

---
+++ js/jobs.js
@@ -0,0 +1,8 @@
+function togglePopup(id) {
+  if (id == 1)
+    document.getElementById("popup1").classList.toggle("active");
+  else if (id == 2)
+    document.getElementById("popup2").classList.toggle("active");
+  else if (id == 3)
+    document.getElementById("popup3").classList.toggle("active");
+}
